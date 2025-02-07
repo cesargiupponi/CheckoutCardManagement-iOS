ad732a3e2ab1b5895e3306b9f4bc3f3ed56e0701
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-02-06 22:17:50 -0300

    Set up to create dynamic library in Package

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "CheckoutCardManagement",
+            type: .dynamic,
             targets: ["CheckoutCardManagement"]),
         .library(
             name: "CheckoutCardManagementStub",
